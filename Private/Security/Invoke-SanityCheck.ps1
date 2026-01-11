1bf0f6eabf0f15d26a7a8509302d714d76a2dc65
Dan Damit <167915895+dan-damit@users.noreply.github.com>
2026-01-11 15:41:54 -0600

    new tool

---
+++ Private/Security/Invoke-SanityCheck.ps1
@@ -0,0 +1,8 @@
+function sanity_check {
+    Write-Host "Running sanity_check..." -ForegroundColor DarkCyan
+    Start-Sleep -Milliseconds 300
+
+    Write-Host "Operator sanity: questionable" -ForegroundColor Yellow
+    Write-Host "Module sanity: excellent" -ForegroundColor Green
+    Write-Host "Proceed with caution." -ForegroundColor DarkYellow
+}
