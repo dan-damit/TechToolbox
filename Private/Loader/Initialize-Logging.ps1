9a5d769663387670dbf8a4b32d4711dc077055de
Dan Damit <167915895+dan-damit@users.noreply.github.com>
2026-01-13 22:58:31 -0600

    finally fixed loading bugs after refactoring to be more modular

--- Private/Loader/Initialize-Logging.ps1
+++ Private/Loader/Initialize-Logging.ps1
@@ -10,12 +10,12 @@ function Initialize-Logging {
     if (-not $cfg) { return }
 
     $logDir = $cfg["paths"]["logs"]
-    $logFile = $cfg["logging"]["logFile"]
+    $logFile = $cfg["settings"]["logging"]["logFile"]
 
     if (-not $logFile -and $logDir) {
         $logFile = Join-Path $logDir ("TechToolbox_{0:yyyyMMdd}.log" -f (Get-Date))
     }
 
-    $script:log['enableConsole'] = $cfg["logging"]["enableConsole"]
+    $script:log['enableConsole'] = $cfg["settings"]["logging"]["enableConsole"]
     $script:log['logFile'] = $logFile
 }
